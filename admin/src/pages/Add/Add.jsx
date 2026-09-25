d9c865de85d38b0c34a51af11ce5519e204b49c6
Shibom Das <shibomdas.it2023@nsec.ac.in>
2026-09-25 21:43:49 +0530

    Fix admin Add component case

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import './Add.css'
+import './add.css'
 import { assets } from '../../assets/assets'
 import axios from "axios"
 import { toast } from 'react-toastify'
